bafa112a1d7ad939e28f86895a2d483f4dded0bf
Dahvid Montes <i202223643@cibertec.edu.pe>
2026-09-07 20:09:24 -0500

    chore: initial spring boot setup

---
+++ src/main/java/com/jhadmontes/high_demand_ticketing_platform/HighDemandTicketingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.jhadmontes.high_demand_ticketing_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighDemandTicketingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighDemandTicketingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhadmontes/high_demand_ticketing_platform/HighDemandTicketingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhadmontes.high_demand_ticketing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighDemandTicketingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
